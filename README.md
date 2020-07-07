5893d48933a5d2f8e4b2072aa3d8434e87c39a4b
Ubuntu <ubuntu@pi4-ubuntu.outsideopen.com>
2020-07-07 19:07:20 +0000

    edited README.md, wifi_quality.py parsing algorithm development cont

--- README.md
+++ README.md
@@ -14,3 +14,5 @@
 - interactive SQL prompt with bash command "sqlite3"
 - using SQLite3 because it is file/disk based and will be easier to use on the USB drive
 - the utils directory will be populated with tasks, with the purpose of independently gathering, parsing, and then storing data upon execution of the script
+- utilities were originally written in bash but moved to python equivalent scripts due to python supporting sqlite3 natively and having far superior string manipulating capabilities
+- the python scripts cannot be run independently of the main program due to it relying on environment variables being exported in "config" file
--- utils/wifi_quality.py
+++ utils/wifi_quality.py
@@ -3,9 +3,11 @@
 # hydrant utility, search for nearby wireless accesspoints
 # script will run through once, and store gathered data in the database
 # command utility to scrape: sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID'
+# gather information: encryption, quality, last beacon, ESSID
 
 import sqlite3
 import os
+import subprocess
 
 table_name = "wifi_quality"
 
@@ -24,9 +26,32 @@ if c.fetchone()[0]==1 :
     print("table exists for {}, continuing".format(table_name))
 else:
     print("no table exists for {}, creating".format(table_name))
-    # create table
+    #c.execute('''CREATE TABLE students (rollno real, name text, class real)''')
 
-			
+# scrape the command line utility
+output = subprocess.run("sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID' | tr -d '\n'", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+#print(output)
+
+# parse the output into desired variables
+# remove spaces and place words into an array
+output = output.split(" ")
+simplified_output = []
+for i in output:
+    if i != '':
+        simplified_output.append(i)
+#print(simplified_output)
+
+# this data is reported in cells, split data into cells
+# using list comprehension + zip() + slicing + enumerate() 
+size = len(simplified_output) 
+idx_list = [idx for idx, val in
+            enumerate(simplified_output) if val == "Cell"] 
+res = [simplified_output[i: j] for i, j in
+        zip([0] + idx_list, idx_list + 
+        ([size] if idx_list[-1] != size else []))]
+res.remove([])
+#print(res)
+
 #commit the changes to db			
 conn.commit()
 #close the connection

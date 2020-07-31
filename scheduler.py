e0f390e38041d3c3c5a36afa250109d3c21abcce
Ubuntu <ubuntu@ubuntu.outsideopen.com>
2020-07-31 21:29:41 +0000

    modified scheduler to work with new format

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-from utils.modules.log import log
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 import yaml
 import time
 import signal
 
 # hydrant main program script, schedule and execute utilities according to configuration
 # does not handle uploading to API
-# run in the background
 
 def signal_handler(signal, frame):
     global interrupted
@@ -32,19 +30,19 @@ class Utility:
 
     # parse YAML config file
     def load_yaml(self, config_path):
-        log("parsing "+config_path)
-        with open("utils/config/"+config_path) as f:
+        print("parsing "+config_path)
+        with open(config_path) as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
             try:
                 self.enabled = data["enabled"]
                 self.exec_time = data["exec_time"]
                 self.exec_duration = data["exec_duration"]
             except KeyError:
-                log("failed to parse "+config_path, error=True)
+                print("failed to parse "+config_path)
 
     def execute(self):
-        log("executing "+self.util_path)
-        os.system("utils/"+self.util_path+" "+str(self.exec_duration))
+        print("executing "+self.util_path)
+        os.system(self.util_path)
         
 
 class Scheduler:
@@ -59,35 +57,28 @@ class Scheduler:
             util = self.queue[0]
             if util.enabled:
                 exec_time = max(0, util.exec_time)          # make sure time is not negative, if it is it will be 0
-                log("waiting exec_time "+str(exec_time))
+                print("waiting exec_time "+str(exec_time))
                 time.sleep(exec_time)
                 util.execute()
             self.queue.remove(util)
 
-log("starting Digital Hydrant")
+print("starting Digital Hydrant")
 
-# load file names
-utility_files = [f for f in listdir("utils") if isfile(join("utils", f)) and f[-3:]==".py"]
-utility_config = [f for f in listdir("utils/config") if isfile(join("utils/config", f)) and f[-4:]==".yml"]
+# load collector directory path names
+# directories to be not added to the collector dir list
+dir_blacklist = ['__pycache__', 'wifi_quality', 'speedtest', 'netdiscover', 'wifi_auth', 'lldp', 'auth_nearest_wifi', 'collector_template']
+dir_list = [f for f in listdir("collectors") if isdir(join("collectors", f)) and f not in dir_blacklist]
 
-# match python file to config file
-# could just assume they have the same filename and catch exceptions, but this filters out files without a matching python/config pair
-utility_config_dict = {}
-for f in utility_files:
-    f_compare = f.replace(f[-3:], '')
-    for ff in utility_config:
-        ff_compare = ff.replace(ff[-4:], '')
-        if f_compare == ff_compare:
-            utility_config_dict[f] = ff
-
+# create utility objects
 utility_list = []
-for i in utility_config_dict:
-    f = utility_config_dict[i]
-    utility_list.append(Utility(i, f))
+for dir in dir_list:
+    main = "collectors/"+dir+"/main.py"
+    config = "collectors/"+dir+"/config.yml"
+    utility_list.append(Utility(main, config))
 
 scheduler = Scheduler(utility_list)
 while 1:
     scheduler.execute_queue()
     if interrupted:
-        log("main loop interrupted, exiting...", error=True)
+        print("main loop interrupted, exiting...")
         break

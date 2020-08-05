35cb9b300efedf863b0cfa3378c9ee91bc41dd34
Ubuntu <ubuntu@ubuntu.outsideopen.com>
2020-08-05 18:59:09 +0000

    moved lldp.py to new format

--- collectors/lldp/main.py
+++ collectors/lldp/main.py
@@ -5,73 +5,51 @@
 # command utility to scrape: lldpctl (-f json?)
 # gather information: SysName, SysDescr, PortID, MgmtIP, vlan-id
 
-################IMPORT STATEMENTS#################
-import sqlite3
-import os
-import subprocess
-import datetime
-import json     # the module being parsed output JSON format, use module to make life easier
-from modules.log import log
-import sys
-from modules.upload import upload
+# import collector module from parent directory
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from collector import Collector
+import json
 
-# load variables from config file
-db_name = os.environ["db_name"]
-drive_path = os.environ["drive_path"]
-# connect to the SQLite3 database
-conn = sqlite3.connect(str(drive_path) + "/" + str(db_name))
-c = conn.cursor()
-# check passed parameters
-if len(sys.argv) != 2:
-    log("timeout left undefined, exiting...", error=True)
-    exit()
-timeout = sys.argv[1]
-##################################################
+# create collector
+collector_name = "lldp"
+collector = Collector(collector_name)
 
-# create table if it does not exist
-table_name = "lldp"
-c.execute('''CREATE TABLE IF NOT EXISTS {} (SYSTEM_NAME TEXT, SYSTEM_DESCRIPTION TEXT, PORT_ID TEXT, MANAGEMENT_IP TEXT, VLAN_ID INTEGER, DATETIME TIMESTAMP)'''.format(table_name))
-
-# scrape the command line utility   # netdiscover either runs indefinetely or just a really long time, a timeout is needed, set in seconds
-log("collecting data for table {}".format(table_name))
-if timeout == "-1":     output = subprocess.run("lldpctl -f json", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-else:                   output = subprocess.run("sudo timeout {} lldpctl -f json".format(timeout), shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+# scrape the command line utility
+command = "lldpctl -f json"
+output = collector.execute(command)
 
 # parse the output into desired variables
 output = json.loads(output)
 try:
     for i in output["lldp"]["interface"]:   # iterate per interface
+        parsed_output = {}
+
         # SysName
         sysname = list(output["lldp"]["interface"][i]["chassis"].keys())[0]
-        #print(sysname)
+        parsed_output["SYSTEM_NAME"] = sysname
 
         # SysDescr
         sysdescr = output["lldp"]["interface"][i]["chassis"][sysname]["descr"]
-        #print(sysdescr)
+        parsed_output["SYSTEM_DESCRIPTION"] = sysdescr
        
         # PortID
         portid = output["lldp"]["interface"][i]["port"]["id"]["type"]
         portid += " " + output["lldp"]["interface"][i]["port"]["id"]["value"]
-        #print(portid)
+        parsed_output["PORT_ID"] = portid
 
         # MgmtIP
         mgmtip = output["lldp"]["interface"][i]["chassis"][sysname]["mgmt-ip"]
-        #print(mgmtip)
+        parsed_output["MANAGEMENT_IP"] = mgmtip
 
         # vlan-id
         vlanid = int(output["lldp"]["interface"][i]["vlan"]["vlan-id"])
-        #print(vlanid, type(vlanid))
+        parsed_output["VLAN_ID"] = vlanid
 
-        # store to table:   # quotes were added to some strings to comply with SQL syntax
-        date = str(datetime.datetime.now())
-        c.execute('''INSERT INTO {} VALUES({}, {}, {}, {}, {}, {})'''.format(table_name, '"'+str(sysname)+'"', '"'+str(sysdescr)+'"', '"'+str(portid)+'"', '"'+str(mgmtip)+'"', vlanid, '"'+str(date)+'"'))
-        conn.commit()
-        upload(table_name, date)
+        # store to table
+        collector.publish(parsed_output)
 
-except KeyError:
-    log("no interface found", error=True)
+except KeyError as err:
+    collector.logger.error("No interface found, with error {}".format(err))
 
-#commit the changes to db			
-conn.commit()
-#close the connection
-conn.close()
+collector.close()

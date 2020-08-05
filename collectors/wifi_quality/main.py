100077ddf45b57ec0c77bac8d80df38b7c04a113
Ubuntu <ubuntu@ubuntu.outsideopen.com>
2020-08-05 21:08:07 +0000

    moved wifi quality to use collector.py

--- collectors/wifi_quality/main.py
+++ collectors/wifi_quality/main.py
@@ -5,36 +5,18 @@
 # command utility to scrape: sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID'
 # gather information: encryption, quality, last beacon, ESSID
 
-################IMPORT STATEMENTS#################
-import sqlite3
-import os
-import subprocess
-import datetime
-import sys
-from modules.log import log
-from modules.upload import upload
+# import collector module from parent directory
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from collector import Collector
 
-# load variables from config file
-db_name = os.environ["db_name"]
-drive_path = os.environ["drive_path"]
-# connect to the SQLite3 database
-conn = sqlite3.connect(str(drive_path) + "/" + str(db_name))
-c = conn.cursor()
-# check passed parameters
-if len(sys.argv) != 2:
-    log("timeout left undefined, exiting...", error=True)
-    exit()
-timeout = sys.argv[1]
-##################################################
-
-# create table if it does not exist
-table_name = "wifi_quality"
-c.execute('''CREATE TABLE IF NOT EXISTS {} (ADDRESS TEXT, ENCRYPTION TEXT, QUALITY TEXT, LAST_BEACON TEXT, ESSID TEXT, DATETIME TIMESTAMP)'''.format(table_name))
+# create collector
+collector_name = "wifi_quality"
+collector = Collector(collector_name)
 
 # scrape the command line utility
-log("collecting data for table {}".format(table_name))
-if timeout == "-1":     output = subprocess.run("sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID' | tr -d '\n'", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-else:                   output = subprocess.run("sudo timeout {} sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID' | tr -d '\n'".format(timeout), shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+command = "sudo iwlist wlan0 scanning | egrep 'Cell |Encryption|Quality|Last beacon|ESSID' | tr -d '\n'"
+output = collector.execute(command)
 
 # parse the output into desired variables
 # remove spaces and place words into an array
@@ -55,15 +37,16 @@ res = [simplified_output[i: j] for i, j in
 res.remove([])
 
 for i in res:
+    parsed_output = {}
     # get variables per cell
 
     # address:
     address = i[i.index("Address:") + 1]
-    #print(address)
+    parsed_output["ADDRESS"] = address
 
     # encryption: 
     encryption = i[i.index("Encryption") + 1]
-    #print(encryption)
+    parsed_output["ENCRYPTION"] = encryption
 
     # quality:
     value = None
@@ -73,11 +56,11 @@ for i in res:
             break
     value = value.split("=")
     quality = value[1]
-    #print(quality)
+    parsed_output["QUALITY"] = quality
 
     # last beacon:
     last_beacon = i[i.index("beacon:") + 1]
-    #print(last_beacon)
+    parsed_output["LAST_BEACON"] = last_beacon
 
     # ESSID:
     value = None
@@ -96,15 +79,10 @@ for i in res:
         value = value[:-1]
     value = value.split(":")
     essid = value[1]
-    #print(essid)
+    essid = essid[:-1][1:]
+    parsed_output["ESSID"] = essid
 
-    # store to table:   # quotes were added to some strings to comply with SQL syntax
-    date = str(datetime.datetime.now())
-    c.execute('''INSERT INTO {} VALUES({}, {}, {}, {}, {}, {})'''.format(table_name,'"'+str(address)+'"', '"'+str(encryption)+'"', '"'+str(quality)+'"', '"'+str(last_beacon)+'"', str(essid), '"'+date+'"'))
-    conn.commit()
-    upload(table_name, date)
+    # store to table
+    collector.publish(parsed_output)
 
-#commit the changes to db
-conn.commit()
-#close the connection
-conn.close()
+collector.close()

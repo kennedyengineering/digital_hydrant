9854ce877ec96ebaa59bc2210e791e1b6df8a810
Ubuntu <ubuntu@ubuntu.outsideopen.com>
2020-08-05 20:25:59 +0000

    moved speedtest to new format

--- collectors/speedtest/main.py
+++ collectors/speedtest/main.py
@@ -5,64 +5,41 @@
 # command utility to scrape: speedtest-cli --simple
 # gather information: ping, download, upload
 
-################IMPORT STATEMENTS#################
-import sqlite3
-import os
-import subprocess
-import datetime
-import sys
-from modules.log import log
-from modules.upload import upload as up
+# import collector module from parent directory
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from collector import Collector
 
-# load variables from config file
-db_name = os.environ["db_name"]
-drive_path = os.environ["drive_path"]
-# connect to the SQLite3 database
-conn = sqlite3.connect(str(drive_path) + "/" + str(db_name))
-c = conn.cursor()
-# check passed parameters
-if len(sys.argv) != 2:
-    log("timeout left undefined, exiting...", error=True)
-    exit()
-timeout = sys.argv[1]
-##################################################
+# create collector
+collector_name = "speedtest"
+collector = Collector(collector_name)
 
-# create table if it does not exist
-table_name = "speedtest"
-c.execute('''CREATE TABLE IF NOT EXISTS {} (PING TEXT, DOWNLOAD TEXT, UPLOAD TEXT, DATETIME TIMESTAMP)'''.format(table_name))
-
-# scrape the command line utility
-log("collecting data for table {}".format(table_name))
-if timeout == "-1":     output = subprocess.run("speedtest-cli --simple", shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-else:                   output = subprocess.run("sudo timeout {} speedtest-cli --simple".format(timeout), shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+# scape the command line utility
+command = "speedtest-cli --simple"
+output = collector.execute(command)
 
 # parse the output into desired variables
 output = output.split("\n")
-ping = ''
-download = ''
-upload = ''
+parsed_output = {}
 for entry in output:
     if entry.find("Ping") != -1:
         ping = entry
         ping = ping.split(": ")
         ping = ping[1]
+        parsed_output["PING"] = ping
+
     elif entry.find("Download") != -1:
         download = entry
         download = download.split(": ")
         download = download[1]
+        parsed_output["DOWNLOAD"] = download
+
     elif entry.find("Upload") != -1:
         upload = entry
         upload = upload.split(": ")
         upload = upload[1]
-#print(ping, download, upload)
+        parsed_output["UPLOAD"] = upload
 
-# store to table:   # quotes were added to some strings to comply with SQL syntax
-date = str(datetime.datetime.now())
-c.execute('''INSERT INTO {} VALUES("{}", "{}", "{}", "{}")'''.format(table_name, str(ping), str(download), str(upload), date))
-conn.commit()
-up(table_name, date)
+collector.publish(parsed_output)
 
-#commit the changes to the database
-conn.commit()
-#close the connection
-conn.close()
+collector.close()

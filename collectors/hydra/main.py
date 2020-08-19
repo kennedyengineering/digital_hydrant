e51cce49878632e71d07dc242eb039bf260cce50
Ubuntu <ubuntu@ubuntu.outsideopen.com>
2020-08-19 21:41:55 +0000

    inital development of hydra collector, modified netdiscover/config.yml to include a longer run time

--- collectors/hydra/main.py
+++ collectors/hydra/main.py
@@ -2,34 +2,77 @@
 
 # hydra collector utility, create target list from netdiscover data and test against default credential word list
 # script will run through once, and store gathered data in the database
-# command utility to scrape: hydra....
-# gather information: accessed [Y/N], found credentials
+# command utility to scrape: hydra
+# gather information: target, output log
 
 
 # import collector module from parent directory
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from collector import Collector
+import socket
+import fcntl
+import struct
 
 # create collector
 collector_name = "hydra"
 collector = Collector(collector_name)
 
-# capital -L for user list, capital -P for password list, do both at the same time? --> exponential execution time...
-# different protocols? start with ssh. wtf is snmp? prob for switches?
-# hydra -L userlist.txt -P wordlist.txt <hostname> ssh
-# hydra -P wordlist.txt <hostname> snmp
+# collect current IP address, and subnet
+iface = collector.misc_config['interface']
+ip_addr =  socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 0x8915, struct.pack('256s', str(iface[:15]).encode('utf-8')))[20:24])
 
-print(collector.misc_config['wordlist_path'])
+def strip_ip(ip):
+    ip_range_list = str(ip).split(".")
+    del ip_range_list[3]
+    return ip_range_list[0]+"."+ip_range_list[1]+"."+ip_range_list[2]+".0"
 
-# scrape the command line utility
-command = ""
-output = collector.execute(command)
+correct_ip = strip_ip(ip_addr)
 
-# organize data into a dictionary for publishing
+# find all IP's that are on the wrong subnet and add to target list
+collector.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='netdiscover';")
+result = collector.cursor.fetchall()
+if len(result) == 0:
+    collector.logger.error("No netdiscover data available, exiting...")
+    collector.close()
+    exit()
+else:
+    collector.logger.debug("Found netdiscover data")
+
+collector.cursor.execute("SELECT IP FROM netdiscover WHERE IP NOT LIKE '{}%'".format(correct_ip[:-2]))
+target_ips = (collector.cursor.fetchall())
+if len(target_ips) == 0:
+    collector.logger.error("No IP's found to be on the wrong subnet, exiting...")
+    collector.close()
+    exit()
+
+# remove duplicate IP's
+target_ips = list(dict.fromkeys(target_ips))
+
+command_ssh_template = "sudo hydra -I -L {} -P {} <hostname> ssh 2>&1".format(collector.misc_config['userlist_path'], collector.misc_config['wordlist_path'])
+command_snmp_template = "sudo hydra -I -P {} <hostname> snmp 2>&1".format(collector.misc_config['wordlist_path'])
+
 parsed_output = {}
-#parsed_output["DHCP_LOG"] = output
-#collector.publish(parsed_output)
+
+for target in target_ips:
+    target = target[0]
+
+    collector.logger.debug("Operating on target {}, starting attack".format(target))
+
+    output_log = ""
+    
+    # scrape the command line utility
+    output = collector.execute(command_ssh_template.replace("<hostname>", target))
+    output_log = output_log + output + "\n"
+    output = collector.execute(command_snmp_template.replace("<hostname>", target))
+    output_log = output_log + output + "\n"
+    
+    # organize data into a dictionary for publishing
+    parsed_output["TARGET"] = target
+    parsed_output["OUTPUT_LOG"] = output_log
+    collector.publish(parsed_output)
+
+    break
 
 collector.close()
 
